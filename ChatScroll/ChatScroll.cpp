83ce40ad5365aa68abdbf8fb79488370b3765115
Pangjae <chris82953@gmail.com>
2023-04-10 11:37:35 +0900

    초기 커밋.

---
+++ ChatScroll/ChatScroll.cpp
@@ -0,0 +1,8 @@
+
+#include <cstdio>
+void main()
+{
+	int num = 1;
+	printf("123gacha");
+	scanf("%d", &num);
+}
---
+++ GachaSimulator/GachaSimulator.cpp
@@ -0,0 +1,9 @@
+
+#include <cstdio>
+
+void main()
+{
+	int num = 1;
+	printf("123gacha");
+	scanf("%d", &num);
+}
