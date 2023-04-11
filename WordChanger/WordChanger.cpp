bcdf86ce6c4330574d15624649bf9b126c6d3718
Pangjae <chris82953@gmail.com>
2023-04-11 23:09:18 +0900

    WordChange 작성

--- WordChanger/WordChanger.cpp
+++ WordChanger/WordChanger.cpp
@@ -1,9 +1,101 @@
+#include <Windows.h>
 #include <cstdio>
+#include <cstring>
+#include <locale>
+using namespace std;
 
+#define MAX_STRLEN 1000
+#define df_WORD_CNT 13
+
+WCHAR g_Dic[df_WORD_CNT][2][20]
+{
+  { L"i",L"나" },{ L"you",L"너" },{ L"like",L"좋아한다" },{ L"love",L"사랑한다" },{ L"hate", L"증오한다" },
+  { L"am",L"은/는" },{ L"are", L"은/는" },{L"is", L"은/는"}, { L"she", L"그녀" },{ L"he", L"그" },
+  { L"boy", L"소년" },{L"a",L""}, { L"an",L"" }
+};
+
+WCHAR g_Buff[MAX_STRLEN / 2][MAX_STRLEN];
+
+
+void StringDiv(PCWCHAR);
+void Exchange(WCHAR* pSrc, WCHAR* pDest);
 void main()
 {
-	printf("dffd");
-	int num = 1;
-	printf("123gacha");
-	scanf("%d", &num);
+	_wsetlocale(LC_ALL, L"kor");
+
+	WCHAR wInput[MAX_STRLEN];
+	WCHAR wOutput[MAX_STRLEN];
+
+	ZeroMemory(wInput, sizeof(wInput));
+	ZeroMemory(wOutput, sizeof(wOutput));
+
+
+	fgetws(wInput, MAX_STRLEN, stdin);
+
+	// 문자열을 소문자로 변환
+
+
+	StringDiv(wInput);
+	for (int i = 0; i < MAX_STRLEN / 2; i++)
+	{
+		if (wcscmp(g_Buff[i], L"\0") != 0)
+		{
+			Exchange(g_Buff[i], wOutput);
+		}
+	}
+
+	wprintf(L"%s\n", wOutput);
+	system("pause");
+
+
+}
+
+void StringDiv(PCWCHAR wstr)
+{
+	int iArr = 0;
+	int iPrev = 0;
+	int iCur = 0;
+	if (wstr[0] == L' ' || wstr[0] == L',' || wstr[0] == L'.')
+	{
+		iCur = 1;
+		//iPrev = 1;
+	}
+
+	for (; iCur < MAX_STRLEN; iCur++)
+	{
+		if (wstr[iCur] == L' ' || wstr[iCur] == L',' || wstr[iCur] == L'.')
+		{
+			if (wstr[iCur - 1] == L' ' || wstr[iCur - 1] == L',' || wstr[iCur - 1] == L'.')
+			{
+				iPrev = iCur + 1;
+				continue;
+			}
+
+			wcsncpy_s(g_Buff[iArr++], wstr + iPrev, iCur - iPrev);
+			iPrev = iCur + 1;
+		}
+
+
+		if (wstr[iCur] == L'\0')
+		{
+			wcsncpy_s(g_Buff[iArr++], wstr + iPrev, iCur - iPrev-1);
+			break;
+		}
+	}
+}
+
+void Exchange(WCHAR* pSrc, WCHAR* pDest)
+{
+	for (int i = 0; i < df_WORD_CNT; i++)
+	{
+		if (wcscmp(g_Dic[i][0], pSrc) == 0)
+		{
+			wcscat_s(pDest, MAX_STRLEN, g_Dic[i][1]);
+			wcscat_s(pDest, MAX_STRLEN, L" ");
+			return;
+		}
+	}
+	wcscat_s(pDest, MAX_STRLEN, pSrc);
+	wcscat_s(pDest, MAX_STRLEN, L" ");
+	
 }
--- WordExtractor/WordExtractor.cpp
+++ WordExtractor/WordExtractor.cpp
@@ -1,9 +1,108 @@
-#include <cstdio>
+/*
+  WORD EXTRACTOR
+* ' ' / '.' / ','
+* 문자열의 각 요소들을 포인터로 접근하여 조작
+* 자른 문자열 요소를 벡터에 넣음
+* 
+*/
+
+
+#include <Windows.h>
+#include <sstream>
+#include <iostream>
+#include <vector>
+#include <string>
+#pragma comment(lib, "Winmm")
+using namespace std;
+
+#define MAX_STRLEN 1000
+
+
+
+void StringDivide(PCWCHAR szString, vector<wstring> &wordArray);
+void PrintWord(vector<wstring> &wordArray);
+
+__int64 GetTick();
+__int64 Begin, End;
 
 void main()
 {
-	printf("dffd");
-	int num = 1;
-	printf("123gacha");
-	scanf("%d", &num);
+	timeBeginPeriod(1);
+	WCHAR wInput[MAX_STRLEN];
+	vector<wstring> wordArray;
+	memset(wInput, 0, sizeof(wInput));
+
+	while (1)
+	{
+		wcout << "Input String: ";
+		fgetws(wInput, MAX_STRLEN, stdin);
+
+		Begin = GetTick();
+		StringDivide(wInput, wordArray);
+		End = GetTick();
+		PrintWord(wordArray);
+		wcout << endl << " (Elapsed Time : " << End - Begin << "ms)" << endl;
+		wordArray.clear();
+	}
+	timeEndPeriod(1);
 }
+
+
+void StringDivide(PCWCHAR szString, vector<wstring>& wordArray)
+{
+	WCHAR tmp[MAX_STRLEN];
+	int iPrePos = 0;
+	int iCurPos = 0;
+
+	// 처음부터 구분자가 입력됐을 경우
+	if (szString[0] == L' ' || szString[0] == L',' || szString[0] == L'.')
+	{
+		iCurPos = 1;
+		iPrePos = 1;
+	}
+
+	for (; iCurPos < MAX_STRLEN; ++iCurPos)
+	{
+
+		if (szString[iCurPos] == L' ' || szString[iCurPos] == L',' || szString[iCurPos] == L'.')
+		{
+			// 구분자가 연속적으로 입력됐을 경우
+			if (szString[iCurPos - 1] == L' ' || szString[iCurPos - 1] == L',' || szString[iCurPos - 1] == L'.')
+			{
+				iPrePos = iCurPos + 1;
+				continue;
+			}
+
+			wcsncpy_s(tmp, szString + iPrePos, iCurPos - iPrePos);
+			//tmp[iCurPos - iPrePos] = L'\0';
+			wordArray.push_back(tmp);
+
+			iPrePos = iCurPos + 1;
+		}
+
+		// NULL 문자를 만나면 break
+		if (szString[iCurPos] == L'\n')
+		{
+			// \n은 무시
+			wcsncpy_s(tmp, szString + iPrePos, iCurPos - iPrePos);
+			//tmp[iCurPos - iPrePos] = L'\0';
+			wordArray.push_back(tmp);
+			break;
+		}
+	}
+}
+
+void PrintWord(vector<wstring>& wordArray)
+{
+	for (auto it = wordArray.begin(); it != wordArray.end(); ++it)
+		wcout << *it << ' ';
+}
+
+
+__int64 GetTick()
+{
+	LARGE_INTEGER liTime;
+	QueryPerformanceCounter(&liTime);
+	return static_cast<__int64>(liTime.QuadPart);
+}
+
